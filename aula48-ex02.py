da707f9fa008f332f6edd07e806c6cc0b6a4c037
romulorm <62728349+romulorm@users.noreply.github.com>
2020-04-16 12:02:23 -0300

    update do ex.02 da aula 48 com desempacotamento.

--- aula48-ex02.py
+++ aula48-ex02.py
@@ -12,6 +12,6 @@ numeros = list()
 for n in range(3):
     numero = int(input('Digite um número: '))
     numeros.append(numero)
-
-resultado = somatres(numeros[0], numeros[1], numeros[2])
+n1, n2, n3 = numeros #desempacotamento
+resultado = somatres(n1, n2, n3)
 print(f'A soma dos três números é {resultado}')
