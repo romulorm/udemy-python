a483fd5be2339f1d3547b5cb9359870174f2cb1b
romulorm <62728349+romulorm@users.noreply.github.com>
2020-04-19 18:36:44 -0300

    Create aula59-exerc-com-duplicatas.py

---
+++ aula59-exerc-com-duplicatas.py
@@ -0,0 +1,49 @@
+"""
+-> É uma lista de listas de números inteiros
+-> As listas internas tem o tamanho de 10 elementos
+-> As listas internas contém números entre 1 a 10 e eles podem ser duplicados
+Exercício
+-> Crie uma função que encontra o primeiro duplicado considerando o segundo
+    número como a duplicação. Retorne a duplicação considerada.
+        Requisitos:
+            A ordem do número duplicado é considerada a partir da segunda
+            ocorrência do número, ou seja, o número duplicado em si.
+            Exemplo:
+                [1, 2, 3, ->3<-, 2, 1] -> 1, 2 e 3 são duplicados (retorne 3)
+                [1, 2, 3, 4, 5, 6] -> Retorne -1 (não tem duplicados)
+"""
+
+lista = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+]
+
+def verifica_dup(lin):
+    lista_temp = list()
+    for n in lin:
+        if n in lista_temp:
+            return n
+            lista_temp.clear()
+            break
+        else:
+            lista_temp.append(n)
+    if lin.count(n) < 2:
+        return -1
+
+
+# PROGRAMA PRINCIPAL
+
+for i in range(0, len(lista)):
+    linha = lista[i]
+    print(f'\33[32mLista {i}:\33[m \33[34m{verifica_dup(linha)}\33[m')
+    print('-=-' * 5)
