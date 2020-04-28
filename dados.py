c521da9c97b50e4a0e38f32243fb4864087f5f79
romulorm <62728349+romulorm@users.noreply.github.com>
2020-04-27 22:46:57 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,25 @@
+produtos = [
+    {'nome': 'Caneta', 'preco': 1.50},
+    {'nome': 'Lápis', 'preco': 1.00},
+    {'nome': 'Tesoura', 'preco': 4.50},
+    {'nome': 'Giz de cera', 'preco': 6.99},
+    {'nome': 'Borracha', 'preco': 1.50},
+    {'nome': 'Canetinha', 'preco': 4.99},
+    {'nome': 'Livro', 'preco': 25.99},
+    {'nome': 'Régua', 'preco': 8.99},
+    {'nome': 'Caderno', 'preco': 19.99}
+]
+
+clientes = [
+    {'nome': 'Eduardo', 'idade': 54},
+    {'nome': 'Maria', 'idade': 55},
+    {'nome': 'José', 'idade': 60},
+    {'nome': 'João', 'idade': 19},
+    {'nome': 'Helen', 'idade': 43},
+    {'nome': 'Simone', 'idade': 49},
+    {'nome': 'Charles', 'idade': 29},
+    {'nome': 'Humberto', 'idade': 89},
+    {'nome': 'Carlos', 'idade': 51}
+]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
